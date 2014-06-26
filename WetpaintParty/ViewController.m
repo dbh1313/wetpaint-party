e6642dd9ab38e65a0398472d0e2237a8c1c38970
David Howe <dhowe@its-imac.corp.local>
2014-06-26 08:27:07 -0700

    Testing

--- WetpaintParty/ViewController.m
+++ WetpaintParty/ViewController.m
@@ -24,7 +24,7 @@
     SKScene * scene = [MyScene sceneWithSize:skView.bounds.size];
     scene.scaleMode = SKSceneScaleModeAspectFill;
     
-    // Present the scene.
+    // Present the scene
     [skView presentScene:scene];
 }
 
